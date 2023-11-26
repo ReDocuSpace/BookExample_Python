a917b3013875493cb5d3857fdfbbb09e42840390
Re_Docu <83597885+ReDocuSpace@users.noreply.github.com>
2023-11-27 05:11:40 +0900

    Introduce_Init

---
+++ test02.py
@@ -0,0 +1,10 @@
+a, b = map(int, input().split())
+
+print(a + b)
+
+
+print(price, "원", sep="")
+print("%10.1f %10s" % (y, animal))
+print(year, month, day, sep="-")
+print("안녕, 나는 {}이고, {}살이야!".format(name, age))
+print("안녕, 나는 {name}이고, {age}살이야!".format(name=name, age=age))
